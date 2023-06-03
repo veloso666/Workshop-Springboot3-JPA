d57091c47f1f6bbe74c14023e5c922fd142cb957
João lucas <jlvatejo123@gmail.com>
2023-06-03 17:51:12 -0300

    Product

---
+++ src/main/java/com/jlvatejo/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.jlvatejo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jlvatejo.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	
+
+}
